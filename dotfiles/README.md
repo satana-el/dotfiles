26a05d12a6c8e83b7355e595ea44814968453f1b
satana-el <135360275+satana-el@users.noreply.github.com>
2023-09-04 22:20:06 +0530

    Add files via upload

---
+++ dotfiles/README.md
@@ -0,0 +1,1 @@
+All the files need for me to setup an arch install with my rice.
---
+++ dotfiles/arch-packages.sh
@@ -0,0 +1,1 @@
+sudo pacman -S zsh bspwm neovim alacritty firefox xorg xorg-xinit sxhkd rofi picom alacritty numlockx lxsession ranger polybar nitrogen
