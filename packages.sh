46297c4b3f3e0a9c8d378e008a836a3c8b56c17e
Li <liladhar@proton.me>
2024-08-19 14:12:22 +0530

    added packages

---
+++ packages.sh
@@ -0,0 +1,3 @@
+sudo pacman -S zsh bspwm neovim alacritty firefox xorg xorg-xinit sxhkd rofi picom alacritty numlockx lxsession ranger polybar nitrogen feh mpv chromium steam flatpak udiskie xclip ttf-nerd-fonts-symbols nvidia-settings interception-caps2esc
+
+chsh -s /bin/zsh
